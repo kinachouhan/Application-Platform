f46d32e3a20bc8e65bc005ea53f6ce57982cdd60
kinachouhan <chouhankina@gmail.com>
2026-01-26 12:14:25 +0530

    Updated CORS

--- Backend/server.js
+++ Backend/server.js
@@ -18,12 +18,27 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://applicationplatform.netlify.app",
+];
+
 app.use(
   cors({
-    origin: "https://applicationplatform.netlify.app",
-    credentials: true,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, 
   })
 );
+
 
 
 
